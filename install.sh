c72cb0ccf40f89143ab0f7b7a37f3543c4f0c99b
Hongyuan <hongyuan0104@gmail.com>
2021-10-26 21:27:15 +0100

    can install & serve

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ruby-full build-essential zlib1g-dev
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
